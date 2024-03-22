546814c863d6c5dd9bb6a9807dccf33fded969c5
Daniel Connelly <dhconnelly@gmail.com>
2024-03-22 23:11:43 +0100

    stage 1, simplify main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,49 +4,33 @@ import java.nio.ByteOrder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-// TODO:
-// 1. extract command line parsing
-// 2. extract statement parsing
-
 public class Main {
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Missing <database path> and <command>");
-            return;
+    static void dbinfo(String path) {
+        try {
+            // TODO: avoid reading the whole file
+            byte[] header = Files.readAllBytes(Path.of(path));
+            short pageSize = ByteBuffer.wrap(header)
+                                       .order(ByteOrder.BIG_ENDIAN)
+                                       .position(16).getShort();
+            System.out.printf("database page size: %d\n", pageSize);
+        } catch (IOException e) {
+            System.out.printf("error reading file: %s\n", e.getMessage());
         }
+    }
 
-        String databaseFilePath = args[0];
+    public static void main(String[] args) {
+        // TODO: extract command line parsing
+        if (args.length < 2) {
+            System.err.printf("usage: sqlite3 <path> <command>\n");
+            System.exit(1);
+        }
+        String path = args[0];
         String command = args[1];
 
+        // TODO: extract statement parsing
         switch (command) {
-            case ".dbinfo" -> {
-                try {
-                    byte[] header = Files
-                            .readAllBytes(Path.of(databaseFilePath));
-
-                    // The page size is stored at the 16th byte offset, using 2
-                    // bytes in big-endian
-                    // order.
-                    // '& 0xFFFF' is used to convert the signed short to an
-                    // unsigned int.
-                    int pageSize = ByteBuffer.wrap(header)
-                            .order(ByteOrder.BIG_ENDIAN).position(16).getShort()
-                            & 0xFFFF;
-
-                    // You can use print statements as follows for debugging,
-                    // they'll be visible
-                    // when running tests.
-                    System.out.println(
-                            "Logs from your program will appear here!");
-
-                    // Uncomment this block to pass the first stage
-                    // System.out.println("database page size: " + pageSize);
-                } catch (IOException e) {
-                    System.out.println("Error reading file: " + e.getMessage());
-                }
-            }
-            default -> System.out
-                    .println("Missing or invalid command passed: " + command);
+            case ".dbinfo" -> dbinfo(path);
+            default -> System.out.printf("invalid command: %s\n", command);
         }
     }
 }
